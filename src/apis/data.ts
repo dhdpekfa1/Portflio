b3b31d6541561c5dcba8d39b6537b58a2f2ebc1d
닮 <dhdpekfa1@daum.net>
2024-12-09 23:39:30 +0900

    [fix] 환경 변수명 변경

--- src/apis/data.ts
+++ src/apis/data.ts
@@ -1,14 +1,13 @@
 import axios from "axios";
-import { TOKEN, DATABASE_ID } from "../../config";
 
 const NOTION_API_BASE_URL = "https://api.notion.com/v1";
-const token = process.env.TOKEN;
-const baseId = process.env.DATABASE_ID;
+const TOKEN = process.env.TOKEN;
+const DATABASE_ID = process.env.DATABASE_ID;
 
 const notionClient = axios.create({
   baseURL: NOTION_API_BASE_URL,
   headers: {
-    Authorization: `Bearer ${token}`,
+    Authorization: `Bearer ${TOKEN}`,
     "Notion-Version": "2022-06-28",
     "Content-Type": "application/json",
   },
@@ -16,7 +15,7 @@ const notionClient = axios.create({
 
 export const getDataList = async () => {
   try {
-    const res = await notionClient.post(`/databases/${baseId}/query`);
+    const res = await notionClient.post(`/databases/${DATABASE_ID}/query`);
 
     if (!res) {
       throw new Error();
