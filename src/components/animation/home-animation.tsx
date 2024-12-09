b17b1dc15451822e99614d9ed02ad8a923009991
닮 <dhdpekfa1@daum.net>
2024-12-09 23:20:20 +0900

    [fix] Lottie dynamic 설정

--- src/components/animation/home-animation.tsx
+++ src/components/animation/home-animation.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import React from "react";
-import Lottie from "react-lottie-player";
+import dynamic from "next/dynamic";
 import lottieJson from "/public/animation/home_animation.json";
+
+const Lottie = dynamic(() => import("react-lottie-player"), { ssr: false });
 
 const HomeAnimationLottie = () => {
   return <Lottie loop animationData={lottieJson} play />;
