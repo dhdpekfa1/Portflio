db1cf329d082232738fce7e2e83e82580f0d3030
닮 <dhdpekfa1@daum.net>
2024-12-09 23:38:07 +0900

    [fix] 환경 변수 확인

--- src/apis/data.ts
+++ src/apis/data.ts
@@ -2,11 +2,13 @@ import axios from "axios";
 import { TOKEN, DATABASE_ID } from "../../config";
 
 const NOTION_API_BASE_URL = "https://api.notion.com/v1";
+const token = process.env.TOKEN;
+const baseId = process.env.DATABASE_ID;
 
 const notionClient = axios.create({
   baseURL: NOTION_API_BASE_URL,
   headers: {
-    Authorization: `Bearer ${TOKEN}`,
+    Authorization: `Bearer ${token}`,
     "Notion-Version": "2022-06-28",
     "Content-Type": "application/json",
   },
@@ -14,7 +16,7 @@ const notionClient = axios.create({
 
 export const getDataList = async () => {
   try {
-    const res = await notionClient.post(`/databases/${DATABASE_ID}/query`);
+    const res = await notionClient.post(`/databases/${baseId}/query`);
 
     if (!res) {
       throw new Error();
@@ -22,6 +24,10 @@ export const getDataList = async () => {
     return res.data.results;
   } catch (err) {
     console.error("Error querying Notion database:", err);
-    throw err;
+    return {
+      props: {
+        projects: [],
+      },
+    };
   }
 };
