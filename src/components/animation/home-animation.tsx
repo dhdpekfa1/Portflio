d8527ef8946ee07a4744b82dc7e33c7454e4ba45
닮 <dhdpekfa1@daum.net>
2024-12-09 23:26:47 +0900

    [fix] tsconfig.json 파일 수정

--- src/components/animation/home-animation.tsx
+++ src/components/animation/home-animation.tsx
@@ -1,9 +1,8 @@
 "use client";
 
 import dynamic from "next/dynamic";
-import lottieJson from "/public/animation/home_animation.json";
-
 const Lottie = dynamic(() => import("react-lottie-player"), { ssr: false });
+import lottieJson from "/public/animation/home_animation.json";
 
 const HomeAnimationLottie = () => {
   return <Lottie loop animationData={lottieJson} play />;
--- src/components/animation/project-animation.tsx
+++ src/components/animation/project-animation.tsx
@@ -1,9 +1,8 @@
 "use client";
 
 import dynamic from "next/dynamic";
-import lottieJson from "/public/animation/default_project_animation.json";
-
 const Lottie = dynamic(() => import("react-lottie-player"), { ssr: false });
+import lottieJson from "/public/animation/default_project_animation.json";
 
 const ProjectAnimationLottie = () => {
   return <Lottie loop animationData={lottieJson} play />;
