811d18ae6d05d60d4348a7314ef4712f0299f4c6
닮 <dhdpekfa1@daum.net>
2025-02-09 14:42:29 +0900

    [design] 프로젝트 레이아웃 수정

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,17 +1,17 @@
-import React from "react";
-import ProjectItem from "@/components/project/project-item";
-import { getDataList } from "@/apis/data";
-import { NotionPage } from "@/types/data";
+import React from 'react';
+import ProjectItem from '@/components/project/project-item';
+import { getDataList } from '@/apis/data';
+import { NotionPage } from '@/types/data';
 
 const ProjectPage = async () => {
   const data: NotionPage[] = await getDataList();
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen px-5 py-8">
-      <h1 className="text-2xl font-bold ml-auto mr-8">
-        프로젝트: <span className="text-point">{data.length}</span>개
+    <div className='flex flex-col items-center justify-center min-h-screen px-4'>
+      <h1 className='text-lg md:text-xl lg:text-2xl font-bold ml-auto mr-8 mt-6 lg:mt-0'>
+        프로젝트: <span className='text-point'>{data.length}</span>개
       </h1>
-      <div className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:w-full gap-8 m-6 py-10 mb-8 items-center justify-center">
+      <div className='grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:w-full gap-6 lg:gap-8 m-6 pb-10 mb-8 items-center justify-center'>
         {data.reverse().map((project) => (
           <ProjectItem key={project.id} data={project} />
         ))}
