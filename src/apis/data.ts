910ffdac2a1d3363ade14ae467c6f17fa3a8aa42
닮 <dhdpekfa1@daum.net>
2025-02-12 15:29:12 +0900

    [feat] 프로젝트 정렬 변경

--- src/apis/data.ts
+++ src/apis/data.ts
@@ -1,6 +1,6 @@
-import axios from "axios";
+import axios from 'axios';
 
-const NOTION_API_BASE_URL = "https://api.notion.com/v1";
+const NOTION_API_BASE_URL = 'https://api.notion.com/v1';
 const TOKEN = process.env.NOTION_TOKEN;
 const DATABASE_ID = process.env.NOTION_DATABASE_ID;
 
@@ -8,21 +8,29 @@ const notionClient = axios.create({
   baseURL: NOTION_API_BASE_URL,
   headers: {
     Authorization: `Bearer ${TOKEN}`,
-    "Notion-Version": "2022-06-28",
-    "Content-Type": "application/json",
+    'Notion-Version': '2022-06-28',
+    'Content-Type': 'application/json',
   },
 });
 
 export const getDataList = async () => {
   try {
-    const res = await notionClient.post(`/databases/${DATABASE_ID}/query`);
+    const res = await notionClient.post(`/databases/${DATABASE_ID}/query`, {
+      sorts: [
+        {
+          property: 'order',
+          direction: 'descending',
+        },
+      ],
+    });
 
     if (!res || !res.data || !res.data.results) {
-      throw new Error("Invalid response structure");
+      throw new Error('Invalid response structure');
     }
+    console.log(res.data);
     return res.data.results;
   } catch (err) {
-    console.error("Error querying Notion database:", err);
+    console.error('Error querying Notion database:', err);
     return [];
   }
 };
@@ -31,12 +39,12 @@ export const getBlockChildren = async (blockId: string) => {
   try {
     const response = await fetch(`/api/notion/${blockId}`);
     if (!response.ok) {
-      throw new Error("Failed to fetch data");
+      throw new Error('Failed to fetch data');
     }
     const data = await response.json();
     return data.results;
   } catch (error) {
-    console.error("Error fetching Notion block children:", error);
+    console.error('Error fetching Notion block children:', error);
     return [];
   }
 };
